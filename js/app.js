e9b0f485da26a54aac352c5561c3d32a3aa68a8a
ahmad.emulator93@gmail.com <ahmad.emulator93@gmail.com>
2021-05-12 13:46:29 +0200

    scope changed

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ function transpose(sphere,cylinder,axis){
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("./serviceWorker.js",{scope:'./lensometer/'})
+      .register("./serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
